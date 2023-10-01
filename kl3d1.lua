972e756362396346f5c982c1bfcd564c1d4c506f
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-01 20:39:34 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -9,91 +9,41 @@ Section:NewButton("Kledi", "kl3d1", function()
 end)
 
 
-Section:NewTextBox("URL", "Enter Url", function(txt)
-    local image = 'image link' -- image you want to import
-    local resolutionX = 32 -- usually it's 32 but it might change depending on the frame?
-    local resolutionY = 32 -- usually it's 32 but it might change depending on the frame?
-    -- epic coding stuf --
-    local grid = nil
-    local s, e = pcall(function()
-    if game.Players.LocalPlayer.PlayerGui:FindFirstChild'MainGui':FindFirstChild'PaintFrame':FindFirstChild'Grid' then
-    grid = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.Grid
-    elseif game.Players.LocalPlayer.PlayerGui:FindFirstChild'PaintFrame':FindFirstChild'GridHolder':FindFirstChild'Grid' then
-    grid = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid
-    else
-    warn('cannot execute script')
-    return
-    end
-    end)
-    if e then
-    local s1, e1 = pcall(function()
-    grid = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid
-    end)
-    if e1 then
-    warn('cannot execute script')
-    return
-    end
-    end
-    local h = game:GetService("HttpService")
-    function getjson(url)
-    local begin = game:HttpGet("https://f818fcf9-3b10-4c92-8176-0bef47a8421d.id.repl.co/get?url="..url)
-    if (begin == 'the file size is too big!') then
-    return 'fstb'
-    else
-    local json = h:JSONDecode(begin)
-    return json
-    end
-    end
-    function import(url)
-    local pixels = getjson(url)
-    local cells = {}
-    local index = 1
-    if (pixels == 'fstb') then
-    game.StarterGui:SetCore(
-    "SendNotification",
-    {
-    Title = "error",
-    Text = "the file size exceeds three megabytes, "
-    .."to prevent people from crashing my vps i have set"
-    .." the cap to amount. sorry for the inconvenience"
-    }
-    )
-    else
-    grid['1'].BackgroundColor3 = Color3.fromRGB(
-    pixels[1][1],
-    pixels[1][2],
-    pixels[1][3]
-    )
-    for y = 1, resolutionX, 1 do
-    for x = 1, resolutionY, 1 do
-    pcall(function()
-    local pixel = pixels[index]
-    index = index + 1 -- index += 1 doesn't work wtf
-    local r = pixels[index][1]
-    local g = pixels[index][2]
-    local b = pixels[index][3]
-    grid[tostring(index)].BackgroundColor3 = Color3.fromRGB(r, g, b)
-    table.insert(cells, pixel)
-    end)
-    end
-    end
-    pcall(function()
-    local pixel = pixels[index]
-    index = index + 1 -- index += 1 doesn't work wtf
-    local r = pixels[index][1]
-    local g = pixels[index][2]
-    local b = pixels[index][3]
-    grid[tostring(index)].BackgroundColor3 = Color3.fromRGB(r, g, b)
-    table.insert(cells, pixel)
-    end)
-    game.StarterGui:SetCore(
-    "SendNotification",
-    {
-    Title = "done",
-    Text = "finished importing, check the drawing grid"
-    }
-    )
-    end
-    end
-    import(image)
+-- Create TextBox for URL input
+local TextBox = Instance.new("TextBox")
+TextBox.Name = "ImageURL"
+TextBox.Size = UDim2.new(0, 200, 0, 30)
+TextBox.Position = UDim2.new(0.5, -100, 0.2, 0)
+TextBox.PlaceholderText = "Enter Image URL"
+TextBox.Parent = ScreenGui
+
+-- Create a TextButton to apply the image
+local ApplyButton = Instance.new("TextButton")
+ApplyButton.Name = "ApplyButton"
+ApplyButton.Size = UDim2.new(0, 100, 0, 30)
+ApplyButton.Position = UDim2.new(0.5, -50, 0.3, 0)
+ApplyButton.Text = "Apply"
+ApplyButton.Parent = ScreenGui
+
+ApplyButton.MouseButton1Click:Connect(function()
+    local imageURL = TextBox.Text
+
+    -- Create the ImageLabel
+    local ImageLabel = Instance.new("ImageLabel")
+    ImageLabel.Size = UDim2.new(0, 32, 0, 32)
+    ImageLabel.Position = UDim2.new(0.5, -16, 0.5, -16)
+    ImageLabel.Image = imageURL
+    ImageLabel.Parent = game.Workspace:WaitForChild("StarvingArtistFrame") -- Replace with the actual frame's name
+
+    -- Optional: Resize the frame to fit the image
+    local frame = ImageLabel.Parent
+    frame.Size = UDim2.new(0, 32, 0, 32)
+
+    -- Optional: Set BackgroundTransparency to 1 for no background
+    frame.BackgroundTransparency = 1
+
+    -- Optional: Set other properties as needed
+    -- frame.OtherProperty = Value
+
+    -- Optional: Handle error if imageURL is not a valid image URL
 end)
