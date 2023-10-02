bb5a0b4c50fa8d7c74ac51355573950db3e88921
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-02 16:53:02 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/1uaxx/yunv2/main/librarysource.lua'))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
