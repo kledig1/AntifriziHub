e30ef36d40bb2eb789aac0295f305617d0acd24e
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-02 16:41:17 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -1,3 +1,3 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
 
 local Window = Library.CreateLib("AntifriziHub", "DarkTheme")
