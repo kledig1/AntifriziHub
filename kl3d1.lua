375a25b17ffe53eec3902fbf696c89f3de8b1bcd
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-02 16:51:20 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -1,3 +1,1 @@
 local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ShaddowScripts/Main/main/Library"))()
-
-local Window = Library.CreateLib("AntifriziHub", "DarkTheme")
