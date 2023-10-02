61036fa966a5c6e42e0d065e333403185e7c788c
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-02 16:56:08 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -1,1 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1uaxx/yunv2/main/librarysource.lua'))()
+
+local Library = initLibrary()
+local Window = Library:Load({name = "xan", sizeX = 425, sizeY = 512, color = Color3.fromRGB(255, 255, 255)})
