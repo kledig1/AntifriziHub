04cfa8f9c47dca2d8b8bf3e46339501eafcd2d48
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-02 16:50:37 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -1,3 +1,3 @@
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
+local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ShaddowScripts/Main/main/Library"))()
 
 local Window = Library.CreateLib("AntifriziHub", "DarkTheme")
