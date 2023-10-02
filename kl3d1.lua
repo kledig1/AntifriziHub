cd3cdd97a74c2d3c270b1fa8d55b99a7df47b477
kledig1 <121384747+kledig1@users.noreply.github.com>
2023-10-02 17:34:55 +0200

    Update kl3d1.lua

--- kl3d1.lua
+++ kl3d1.lua
@@ -1,9 +1,21 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/joeengo/exploiting/main/EngoUILIB_V2.lua", true))()
 
+-- Create main window with minimize, maximize, and kill buttons
 local main = library:CreateMain("AntifriziHub", "1.0", Enum.KeyCode.LeftAlt)
+main:CreateButton("Minimize", function()
+    main:SetVisible(false)
+end)
+main:CreateButton("Maximize", function()
+    main:SetVisible(true)
+end)
+main:CreateButton("Kill", function()
+    main:Destroy()
+end)
 
 local tab = main:CreateTab("ImageURL")
 
 tab:CreateLabel("Main")
 
 tab:CreateToggle("Toggle", function(value)
+    -- Your toggle function here
+end)
